97701211b3399351491211a14e43a4731d8778f0
Lamnp <phuclam6987@gmail.com>
2025-11-14 23:10:58 +0700

    fix vercel failed

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://api-class-o1lo.onrender.com/api/lamnp/cart",
   headers: {
     "Content-Type": "application/json",
   },
